998856d9f93611faa56efa00feb18cd649da32d2
Ross Lancaster <toastymallows@gmail.com>
2017-12-04 10:55:39 -0500

    Reorganized drocer.js to use an object, removed onclick HTML attributes

--- app/static/js/drocer.js
+++ app/static/js/drocer.js
@@ -1,117 +1,112 @@
-$(function() {
+function drocerApp(settings) {
 
-    drocer = {};
-    drocer.action = {};
-    drocer.callback = {};
-    drocer.ui = {};
-    drocer.settings = {};
-    drocer.state = {};
-    drocer.settings.search_input_element = 'search-input';
-    drocer.settings.search_results_element = 'search-results';
-    drocer.settings.search_result_control_element = 'search-results-control';
-    drocer.settings.search_result_control_element_shadow = 'search-results-control-shadow';
-    drocer.settings.page_image_container = 'page-image-container';
-    drocer.settings.page_image_element = 'page-image';
-    drocer.settings.page_overlay_element = 'page-image-overlay';
-    drocer.settings.spinner = '<div class="preloader-wrapper big active"><div class="spinner-layer spinner-green-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>';
-    drocer.action.search = function() {
-        var results_container = document.getElementById(drocer.settings.search_results_element);
-        $(results_container).html(drocer.settings.spinner);
+    var _settings = settings;
+
+    $(window).scroll(result_controls_update);
+
+    var search_button = $("#search_button");
+    var match_previous_button = $("#match_previous_button");
+    var match_next_button = $("#match_next_button");
+    var page_previous_button = $("#page_previous_button");
+    var page_next_button = $("#page_next_button");
+
+    search_button.click(search);
+
+    match_previous_button.click(match_previous);
+    match_next_button.click(match_next);
+
+    page_previous_button.click(page_previous);
+    page_next_button.click(page_next);
+
+
+    function search() {
+        var results_container = document.getElementById(_settings.search_results_element);
+        $(results_container).html(_settings.spinner);
         $.post(
-            './search', { q: $('#' + drocer.settings.search_input_element).val() },
-            drocer.callback.search,
+            './search', { q: $('#' + _settings.search_input_element).val() },
+            searchCallback,
             'json'
         );
     };
-    drocer.callback.search = function(response) {
+
+    function searchCallback(response) {
         window.DEBUG_SEARCH = response; //debug
         if (response.matches.length > 0) {
-            drocer.ui.render_search_results(response.matches);
-            drocer.ui.result_controls_show();
+            render_search_results(response.matches);
+            result_controls_show();
         } else {
-            $(document.getElementById(drocer.settings.search_results_element)).html('No results.');
-            drocer.ui.result_controls_hide();
+            $(document.getElementById(_settings.search_results_element)).html('No results.');
+            result_controls_hide();
         }
-    };
-    drocer.action.overlay_click = function() {
-        var ov = document.getElementById(drocer.settings.page_overlay_element);
-        ov.style.background = 'rgba(255, 255, 165, 1)';
-        var box_text_html = drocer.state.match_box_text.replace('\n', '<br>');
-        $(ov).html(box_text_html);
-        $(ov).unbind('click');
-        ov.title = '';
-    };
-    drocer.action.page_previous = function() {
-        drocer.ui.page_change(-1);
-    };
-    drocer.action.page_next = function() {
-        drocer.ui.page_change(1);
-    };
-    drocer.ui.page_change = function(direction) {
-        var current_page = parseInt(drocer.state.page_number);
+    }
+
+    function page_previous() {
+        page_change(-1);
+    }
+
+    function page_next() {
+        page_change(1);
+    }
+
+    function page_change(direction) {
+
+        var current_page = parseInt(drocerState.page_number);
         if (current_page == 1 && direction < 0) {
             return;
         }
-        drocer.state.page_number = current_page + direction;
-        var page_image_url = './page_images/' + drocer.state.document_name + '-' + drocer.state.page_number + '.png';
+
+        drocerState.page_number = current_page + direction;
+        var page_image_url = './page_images/' + drocerState.document_name + '-' + drocerState.page_number + '.png';
         var page_image = new Image();
         page_image.src = page_image_url;
-        page_image.id = drocer.settings.page_image_element;
+        page_image.id = _settings.page_image_element;
         page_image.style.zIndex = 900;
-        var page_image_container = document.getElementById(drocer.settings.page_image_container);
+        var page_image_container = document.getElementById(_settings.page_image_container);
         $(page_image_container).html(page_image);
+
         // hide overlay
-        var ov = document.getElementById(drocer.settings.page_overlay_element);
+        var ov = document.getElementById(_settings.page_overlay_element);
         ov.style.height = '0px';
         ov.style.width = '0px';
+
         // reset scroll
-        drocer.ui.scroll_to(0, 0);
-    };
-    drocer.action.match_previous = function() {
-        drocer.ui.match_change(-1);
-    };
-    drocer.action.match_next = function() {
-        drocer.ui.match_change(1);
-    };
-    drocer.ui.match_change = function(direction) {
-        if (drocer.state.match_number) {
-            var match_number = parseInt(drocer.state.match_number);
-            match_number += direction;
-            var selector = '[data-drocer-match-number=' + match_number + ']';
-            if ($(selector).length) {
-                $(selector).click();
-                drocer.state.match_number = match_number;
-            }
+        scroll_to(0, 0);
+    }
+
+    function match_previous() {
+        match_change(-1);
+    }
+
+    function match_next() {
+        match_change(1);
+    }
+
+    function match_change(direction) {
+        if (!drocerState.match_number) {
+            return;
         }
-    };
-    drocer.ui.scroll_to = function(x, y) {
-        window.scrollTo(x, y);
-        drocer.ui.result_controls_update();
-    };
-    drocer.ui.result_controls_show = function() {
-        $(document.getElementById(drocer.settings.search_result_control_element)).show();
-        $(document.getElementById(drocer.settings.search_result_control_element_shadow)).show();
-    };
-    drocer.ui.result_controls_hide = function() {
-        $(document.getElementById(drocer.settings.search_result_control_element)).hide();
-        $(document.getElementById(drocer.settings.search_result_control_element_shadow)).hide();
-    };
-    drocer.ui.result_controls_update = function() {
-        var control = document.getElementById(drocer.settings.search_result_control_element);
-        var shadow = document.getElementById(drocer.settings.search_result_control_element_shadow);
-        var body_box = document.getElementsByTagName('body')[0].getBoundingClientRect();
-        control.style.top = 0 - body_box.top + 25 + 'px';
-        control.style.left = 0 - body_box.left + 325 + 'px';
-        shadow.style.top = 0 - body_box.top + 25 + 3 + 'px';
-        shadow.style.left = 0 - body_box.left + 325 + 3 + 'px';
-        shadow.style.height = control.getBoundingClientRect().height + 'px';
-        shadow.style.width = control.getBoundingClientRect().width + 'px';
-    };
-    $(document).ready(function() {
-        $(window).scroll(drocer.ui.result_controls_update);
-    });
-    drocer.ui.render_search_results = function(results) {
-        var results_container = document.getElementById(drocer.settings.search_results_element);
+
+        var match_number = parseInt(drocerState.match_number);
+        match_number += direction;
+        var selector = '[data-drocer-match-number=' + match_number + ']';
+        if ($(selector).length) {
+            $(selector).click();
+            drocerState.match_number = match_number;
+        }
+
+    }
+
+    function overlay_click() {
+        var ov = document.getElementById(_settings.page_overlay_element);
+        ov.style.background = 'rgba(255, 255, 165, 1)';
+        var box_text_html = drocerState.match_box_text.replace('\n', '<br>');
+        $(ov).html(box_text_html);
+        $(ov).unbind('click');
+        ov.title = '';
+    }
+
+    function render_search_results(results) {
+        var results_container = document.getElementById(_settings.search_results_element);
         var ul = document.createElement('ul');
         ul.className = 'collection';
         var match_number = 1;
@@ -144,10 +139,10 @@ $(function() {
                         var document_name = JSON.parse(this.getAttribute('data-drocer-document-name'));
                         var box = JSON.parse(this.getAttribute('data-drocer-box'));
                         // update application state
-                        drocer.state.document_name = document_name;
-                        drocer.state.page_number = box.page_number;
-                        drocer.state.match_number = this.getAttribute('data-drocer-match-number');
-                        drocer.state.match_box_text = box.text;
+                        drocerState.document_name = document_name;
+                        drocerState.page_number = box.page_number;
+                        drocerState.match_number = this.getAttribute('data-drocer-match-number');
+                        drocerState.match_box_text = box.text;
                         $('[data-drocer-match-number]').css('background-color', '');
                         $(this).css('background-color', '#a5d6a7');
                         // load page image
@@ -155,12 +150,14 @@ $(function() {
                         var page_image = new Image();
                         page_image.onload = function() {
                             // position overlay on match box
-                            var ov = document.getElementById(drocer.settings.page_overlay_element);
+                            var ov = document.getElementById(_settings.page_overlay_element);
                             ov.title = 'Click to select text.';
                             ov.style.background = 'rgba(255, 255, 0, 0.35)';
                             $(ov).html('');
-                            $(ov).bind('click', drocer.action.overlay_click);
-                            page_box = drocer.ui.box_to_page(box);
+                            $(ov).bind('click', overlay_click);
+
+                            page_box = box_to_page(box);
+
                             ov.style.height = page_box.height + 'px';
                             ov.style.width = page_box.width + 'px';
                             ov.style.top = page_box.top + 'px';
@@ -169,34 +166,38 @@ $(function() {
                             var scroll_x = page_box.left - 325; // aesthetic offset(25) + menu width offset(300)
                             var scroll_y = page_box.top - 175; // aesthetic offset(25) + control height offset (175)
                             //console.log('scrolling: window.scrollTo('+scroll_x+','+scroll_y+')');//debug
-                            drocer.ui.scroll_to(scroll_x, scroll_y);
+
+                            scroll_to(scroll_x, scroll_y);
                         }
                         page_image.src = page_image_url;
-                        page_image.id = drocer.settings.page_image_element;
+                        page_image.id = _settings.page_image_element;
                         page_image.style.zIndex = 900;
-                        var page_image_container = document.getElementById(drocer.settings.page_image_container);
+                        var page_image_container = document.getElementById(_settings.page_image_container);
                         $(page_image_container).html(page_image);
                         window.DEBUG_BOX = box; //debug
                     };
                     p.appendChild(txt);
-                    p.appendChild(drocer.ui.micro_page(box));
+
+                    p.appendChild(micro_page(box));
+
                     a.appendChild(p);
                     li.appendChild(a);
                     match_number++;
                 }
             }
         }
-        drocer.state.match_count = match_number;
+        drocerState.match_count = match_number;
         $(results_container).html(ul);
-    };
+    }
+
     /**
      * Convert PDF BBox coordinates to page image coordinates.
      * @param box DrocerBox with x0,y0,x1,y1 in points (lower-left origin).
      * @returns Object with top, left, height, and width in px (top-left origin).
      * 
      */
-    drocer.ui.box_to_page = function(box) {
-        drocer.ui.scroll_to(0, 0); // reset page offset; simplifies location calculation
+    function box_to_page(box) {
+        scroll_to(0, 0); // reset page offset; simplifies location calculation
         // note: images converted at 175 dpi are 1466px x 1903px
         var img_rect = document.getElementById('page-image').getBoundingClientRect();
         var x_scale = 175 / 72; // convert dpi / source dpi
@@ -214,13 +215,40 @@ $(function() {
             height: page_y(box.y0) - page_y(box.y1),
             width: page_x(box.x1) - page_x(box.x0)
         }
-    };
+    }
+
+    function scroll_to(x, y) {
+        window.scrollTo(x, y);
+        result_controls_update();
+    }
+
+    function result_controls_show() {
+        $(document.getElementById(_settings.search_result_control_element)).show();
+        $(document.getElementById(_settings.search_result_control_element_shadow)).show();
+    }
+
+    function result_controls_hide() {
+        $(document.getElementById(_settings.search_result_control_element)).hide();
+        $(document.getElementById(_settings.search_result_control_element_shadow)).hide();
+    }
+
+    function result_controls_update() {
+        var control = document.getElementById(_settings.search_result_control_element);
+        var shadow = document.getElementById(_settings.search_result_control_element_shadow);
+        var body_box = document.getElementsByTagName('body')[0].getBoundingClientRect();
+        control.style.top = 0 - body_box.top + 25 + 'px';
+        control.style.left = 0 - body_box.left + 325 + 'px';
+        shadow.style.top = 0 - body_box.top + 25 + 3 + 'px';
+        shadow.style.left = 0 - body_box.left + 325 + 3 + 'px';
+        shadow.style.height = control.getBoundingClientRect().height + 'px';
+        shadow.style.width = control.getBoundingClientRect().width + 'px';
+    }
 
     /**
      * Create a tiny div showing the location of a box in a page.
      *
      */
-    drocer.ui.micro_page = function(box) {
+    function micro_page(box) {
         var mp_height = 38;
         var mp_width = 28;
         var mp_scale_y = (175 / 72) * (mp_height / 1903);
@@ -259,6 +287,32 @@ $(function() {
         window.DEBUG_MP_BOX_DIV = mp_box_div;
         return mp_div;
     }
-});
+}
 
-var drocer;
+var drocerState;
+
+$(function() {
+
+    drocerState = {
+        match_box_text: "",
+        page_number: null,
+        match_number: null,
+        document_name: "",
+        match_count: null
+    };
+
+    var settings = {
+        search_input_element: 'search-input',
+        search_results_element: 'search-results',
+        search_result_control_element: 'search-results-control',
+        search_result_control_element_shadow: 'search-results-control-shadow',
+        page_image_container: 'page-image-container',
+        page_image_element: 'page-image',
+        page_overlay_element: 'page-image-overlay',
+        spinner: '<div class="preloader-wrapper big active"><div class="spinner-layer spinner-green-only"><div class="circle-clipper left"><div class="circle"></div></div><div class="gap-patch"><div class="circle"></div></div><div class="circle-clipper right"><div class="circle"></div></div></div></div>'
+    };
+
+
+    var drocer = new drocerApp(settings);
+
+});
